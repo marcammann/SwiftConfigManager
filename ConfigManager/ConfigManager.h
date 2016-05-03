63fe907723120a43eb3e3ceacfbda13dc7684a68
Marc Ammann <mammann@hugeinc.com>
2016-05-02 23:02:30 -0700

    Initial Commit

---
+++ ConfigManager/ConfigManager.h
@@ -0,0 +1,19 @@
+//
+//  ConfigManager.h
+//  ConfigManager
+//
+//  Created by Marc Ammann on 5/2/16.
+//  Copyright © 2016 Codesofa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ConfigManager.
+FOUNDATION_EXPORT double ConfigManagerVersionNumber;
+
+//! Project version string for ConfigManager.
+FOUNDATION_EXPORT const unsigned char ConfigManagerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ConfigManager/PublicHeader.h>
+
+
